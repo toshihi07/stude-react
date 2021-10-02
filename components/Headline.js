2fff55277934e3945ecdc28e07beecb10ca0b6a0
shinmachi_th@stoneb.jp <toshihi0717@gmail.com>
2021-10-02 14:53:15 +0900

    ♻️ スタイルファイルの分割

--- components/Headline.js
+++ components/Headline.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import classes from "./Headline.module.css";
 
 export function Headline(props) {
   return (
     <div>
-      <h1 className={styles.title}>Hello {props.title}</h1>
-      <p className={styles.description}>
+      <h1 className={classes.title}>Hello {props.title}</h1>
+      <p className={classes.description}>
         Get started by editing {props.children}
       </p>
     </div>
--- components/Main.js
+++ components/Main.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import classes from "./Main.module.css";
 import { Headline } from "./Headline";
 import { Links } from "./Links";
 
 export function Main(props) {
   console.log(props);
   return (
-    <main className={styles.main}>
+    <main className={classes.main}>
       <Headline title={props.title}>
-        <code className={styles.code}>pages/{props.page}.js</code>
+        <code className={classes.code}>pages/{props.page}.js</code>
       </Headline>
       <Links />
     </main>
